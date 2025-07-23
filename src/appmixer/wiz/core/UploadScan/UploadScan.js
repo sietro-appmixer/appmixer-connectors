66715b96569d1fb0f62cd22cd22c15f56c38f0eb
vladimir talas <vladimir.talas@gmail.com>
2025-07-23 17:13:55 +0200

    init

--- src/appmixer/wiz/core/UploadScan/UploadScan.js
+++ src/appmixer/wiz/core/UploadScan/UploadScan.js
@@ -1,5 +1,12 @@
 const lib = require('../../lib');
+const crypto = require('crypto');
+const uuid = require('uuid').v4;
 const moment = require('moment');
+
+const generateContentHash = (content) => {
+    const contentString = typeof content === 'string' ? content : JSON.stringify(content);
+    return crypto.createHash('sha256').update(contentString).digest('hex').substring(0, 16);
+};
 
 const getLockConfiguration = (context) => {
 
@@ -15,8 +22,7 @@ module.exports = {
     start(context) {
 
         context.log({
-            step: 'start',
-            lockConfiguration: getLockConfiguration(context)
+            step: 'start', lockConfiguration: getLockConfiguration(context)
         });
 
         const { scheduleValue } = context.properties;
@@ -30,41 +36,80 @@ module.exports = {
 
         const { threshold, scheduleValue } = context.properties;
 
+        if (context.messages.timeout) {
+            await this.scheduleDrain(context);
+            await this.prepareForSend(context, { threshold });
+        } else {
+            const { document, filename, integrationId } = context.messages.in.content;
+
+            if (!await context.stateGet('metadata')) {
+                await context.stateSet('metadata', { filename, integrationId });
+            }
+
+            // await context.stateAddToSet('documents', { id: generateContentHash(document), data: document });
+            await context.stateAddToSet('documents', { id: uuid(), data: document });
+            const entries = await context.stateGet('documents') || [];
+
+            context.log({ step: 'receive', entries: entries.length });
+
+            if (!scheduleValue || (threshold && entries.length >= threshold)) {
+                await this.prepareForSend(context, { threshold });
+            }
+        }
+    },
+
+    async prepareForSend(context, { threshold }) {
+
+        const entriesToUpload = await context.stateSet('documents-upload-batch');
+        if (entriesToUpload) {
+            context.log({
+                step: 'in progress',
+                message: `Found ${entriesToUpload.length} documents in documents-upload-batch.`
+            });
+            return;
+        }
+
         let lock;
         try {
 
             // https://docs.appmixer.com/6.0/v4.1/component-definition/behaviour#async-context.lock-lockname-options
             lock = await context.lock(context.componentId, getLockConfiguration(context));
-
-            const documents = await context.stateGet('documents') || [];
-
-            if (context.messages.timeout) {
-                await this.scheduleDrain(context);
-                if (documents.length > 0) {
-                    const integrationId = await context.stateGet('integrationId');
-                    const filename = await context.stateGet('filename');
-                    await this.sendDocuments(context, { documents, filename, integrationId });
-                }
-                return;
+            const { integrationId, filename } = await context.stateGet('metadata') || {};
+            if (!integrationId || !filename) {
+                throw new context.CancelError('No metadata found in state. Cannot send documents.');
             }
 
-            const { document, filename, integrationId } = context.messages.in.content;
-            documents.push(document);
-            context.log({ step: 'documents in state:', count: documents.length });
-            await context.stateSet('documents', documents);
-            await context.stateSet('filename', filename);
-            await context.stateSet('integrationId', integrationId);
+            const entriesToUpload = await context.stateSet('documents-upload-batch');
+            let documents = [];
 
-            if (threshold && documents.length >= threshold) {
-                await this.sendDocuments(context, { documents, filename, integrationId });
-            } else if (!threshold && !scheduleValue) {
-                await this.sendDocuments(context, { documents, filename, integrationId });
+            if (entriesToUpload) {
+                documents = entriesToUpload.map(entry => entry.data);
+                context.log({
+                    step: 'documents-upload-batch docs',
+                    message: `Prepared ${documents.length} documents for upload.`,
+                    lock
+                });
+
+            } else {
+                const entries = (await context.stateGet('documents') || []);
+
+                await context.stateSet('documents-upload-batch', entries);
+                await context.stateUnset('documents');
+                context.log({
+                    step: 'prepareForSend', message: `Prepared ${entries.length} documents for upload.`, lock
+                });
+                documents = entries.map(entry => entry.data);
             }
+
+            await this.sendDocuments(context, {
+                documents,
+                filename,
+                integrationId
+            });
+            await context.stateUnset('documents-upload-batch');
 
         } finally {
-            if (lock) {
-                lock.unlock();
-            }
+            lock?.unlock();
         }
     },
 
@@ -89,11 +134,14 @@ module.exports = {
 
     async sendDocuments(context, { documents, filename, integrationId }) {
 
-
-
         // only for testing purposes
-        await new Promise(r => setTimeout(r, 10000));
-        return context.sendJson({  }, 'out');
+        context.log({
+            step: 'sendDocuments',
+            message: `Sending ${documents.length} documents for integration ${integrationId} with filename ${filename}.`,
+            documents: documents.map(doc => doc.id)
+        });
+        await new Promise(r => setTimeout(r, 20000));
+        return context.sendJson({}, 'out');
 
         //
         // const { url, systemActivityId } = await lib.requestUpload(context, { filename });
@@ -106,7 +154,6 @@ module.exports = {
         // await lib.uploadFile(context, { url, fileContent });
         // const systemActivity = await lib.getStatus(context, systemActivityId);
         //
-        // await context.stateUnset('documents');
         // // throw error if the system activity is not valid.
         // lib.validateUploadStatus(context, { systemActivity });
         //
