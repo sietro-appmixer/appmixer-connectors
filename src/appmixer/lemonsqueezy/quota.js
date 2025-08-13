c0a949671ab882678d198cd73120fba45c6cc6d2
Sayam Nasir <sayam.nasir53@gmail.com>
2025-08-13 20:56:21 +0500

    Lemonsqueezy Update of icons (#688)

--- src/appmixer/lemonsqueezy/quota.js
+++ src/appmixer/lemonsqueezy/quota.js
@@ -6,7 +6,6 @@ module.exports = {
         {
             limit: 5,
             window: 1000,
-            throttling: 'auto',
             queueing: 'fifo',
             resource: 'requests'
         }
